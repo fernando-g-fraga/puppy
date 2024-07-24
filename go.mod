e14044d8c1fa02909117b0ec02fb251b5f354d01
fernando <fernandogrfraga@gmail.com>
2024-07-24 11:34:39 -0300

    added big barks

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fernando-g-fraga/puppy
 
 go 1.22.5
+
+require github.com/fernando-g-fraga/dog v0.0.0-20240724143030-2b0c8cff7078 // indirect
--- puppy.go
+++ puppy.go
@@ -1,10 +1,21 @@
 // git
 package puppy
 
+import (
+	"github.com/fernando-g-fraga/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Barks())
+}
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
